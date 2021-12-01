ce39b9a45454b6a5222f852b30f4369d07cff5b4
Trang Doan <doantranghp2000@gmail.com>
2021-12-01 23:46:28 +0100

    new approach: use plugin; please only pay attention to plugin_test folder

---
+++ plugin_test/index.php
@@ -0,0 +1,43 @@
+<?php
+    
+    /*
+   Plugin Name:Teeth lover impact counter
+   description: >-
+  a plugin to create awesomeness and spread joy
+   Version: 1.0
+   Author: Teethlovers team
+   License: GPL2
+   */
+
+     require __DIR__ . '/vendor/autoload.php';
+
+     use Automattic\WooCommerce\Client;
+
+     function fetch_data() {
+          $woocommerce = new Client(
+               'https://testshop.markbecker.one', // Your store URL
+               'ck_56bfed484edb55c925f60ca0b70c0f38e7ecb96b', // Your consumer key
+               'cs_835eafa612f3008a62c684ffc703ab41ee590eab', // Your consumer secret
+               [
+                    'wp_api' => true, // Enable the WP REST API integration
+                    'version' => 'wc/v3' // WooCommerce WP REST API version
+               ]
+          );
+          $query = [
+               'date_min' => '2016-05-03', 
+               //TODO, get date_max to get day today
+               'date_max' => '2022-05-04'
+               ];
+
+          $response = $woocommerce->get('reports/sales', $query);
+          $relevant = $response[0];
+          $items = $relevant -> {'total_items'};
+          return $items;
+     }
+     function test_hello_world() {
+          $num_items = fetch_data();
+          $num_str = strval($num_items);
+          return ("the number is " .$num_str);
+     }
+     add_shortcode('example_tag', 'test_hello_world');
+?>
---
+++ plugin_test/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '365d7e4669ef1cc5dce7d7ee097b1f2d68ae3fdb',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '365d7e4669ef1cc5dce7d7ee097b1f2d68ae3fdb',
+            'dev_requirement' => false,
+        ),
+        'automattic/woocommerce' => array(
+            'pretty_version' => '3.0.0',
+            'version' => '3.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../automattic/woocommerce',
+            'aliases' => array(),
+            'reference' => 'a71aa95cc3de3f1d68c6303525d03c0557a96137',
+            'dev_requirement' => false,
+        ),
+    ),
+);
