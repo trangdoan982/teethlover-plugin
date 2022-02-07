91b64d0ab43191dab99a8eb0bb73d7e96764506a
Nahom Agize <nahom@minerva.kgi.edu>
2022-02-07 11:15:27 +0100

    final changes

--- plugin_test/index.php
+++ plugin_test/index.php
@@ -39,7 +39,7 @@
           $with = fetch_data();
 
           ob_start();
-          include('counter.html');
+          include('impact_calculator.html');
           $ob = ob_get_clean();
 
           echo str_replace($replace, $with, $ob);
